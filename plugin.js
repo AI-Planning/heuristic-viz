68162b71fba4d0855c7550162ba138d856c60320
CaitlinAspinall <44043105+CaitlinAspinall@users.noreply.github.com>
2021-03-27 10:54:49 -0400

    Add files via upload

--- plugin.js
+++ plugin.js
@@ -458,7 +458,70 @@ function loadHeuristicData(node){
   var graph2 = makeGraph(dom, prob, child);
   var heuristic = autoUpdate(graph2);
   console.log('----------------------');
+  visual(heuristic, graph2);
 }
+
+function visual(heuristic, graph){
+  console.log(graph);
+
+  var view = d3.select("svg"),
+    width = +view.attr("width"),
+    height = +view.attr("height");
+
+  var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+  var simulation = d3.forceSimulation()
+    .force("link", d3.forceLink().id(function(d) { return d.id; }))
+    .force("charge", d3.forceManyBody())
+    .force("center", d3.forceCenter(width / 2, height / 2));
+
+  var link = view.append("g")
+    .attr("class", "link")
+    .selectAll("line")
+    .data(graph.links)
+    .enter().append("line")
+    .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+  var node = view.append("g")
+    // .attr("class", "nodes")
+    .selectAll("g")
+    .data(graph.nodes)
+    .enter().append("g")
+
+  var circles = node.append("circle")
+    .attr("r", 5)
+    .attr("fill", function(d) { return color(d.group); });
+    // .call(d3.drag();
+        // .on("start", dragstarted)
+        // .on("drag", dragged)
+        // .on("end", dragended));
+  var lables = node.append("text")
+        .text(function(d) {
+          return d.id;
+        })
+        .attr('x', 6)
+        .attr('y', 3);
+
+    node.append("title").text(function(d) { return d.id; });
+
+    simulation.nodes(graph.nodes).on("tick", ticked);
+
+    simulation.force("link").links(graph.links);
+
+}
+function ticked() {
+    link
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node
+        .attr("transform", function(d) {
+          return "translate(" + d.x + "," + d.y + ")";
+        })
+  }
+
 function processDomain(domain) {
     console.log(domain);
 }
